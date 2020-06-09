e87303f57d38862739a8b137df4cf085ec9b6cc8
Samuel da Costa Araujo Nunes <samueldacostaaraujonunes@gmail.com>
2020-06-09 14:43:40 -0300

    Add codes

---
+++ Gate/Gate.ino
@@ -0,0 +1,150 @@
+/*
+ *   AUTHOR:  Samuel da Costa Araujo Nunes, Sinric Developers.
+ *   SINCE: May 2020
+ *   VERSION: v0.1
+*/
+
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
+#include <WebSocketsClient.h> //  https://github.com/kakopappa/sinric/wiki/How-to-add-dependency-libraries
+#include <ArduinoJson.h> // https://github.com/kakopappa/sinric/wiki/How-to-add-dependency-libraries
+#include <StreamString.h>
+
+ESP8266WiFiMulti WiFiMulti;
+WebSocketsClient webSocket;
+WiFiClient client;
+
+#define MyApiKey "" // TODO: Change to your sinric API Key. Your API Key is displayed on sinric.com dashboard
+#define MySSID "" // TODO: Change to your Wifi network SSID
+#define MyWifiPassword "" // TODO: Change to your Wifi network password
+#define HEARTBEAT_INTERVAL 300000 // 5 Minutes 
+
+int gateRelay = 5; //Door D2, responsible for triggering the relay.
+int ledClose = 4; //Door D1, Led on the gate controller board that indicates when the gate is closed.
+int ledOpen = 16; //Door D0, Led on the gate controller board that indicates when the gate is open.
+uint64_t heartbeatTimestamp = 0;
+bool isConnected = false;
+
+void pulsegateRelay(){ //Generates a pulse
+  digitalWrite(gateRelay, HIGH);
+  delay(120);
+  digitalWrite(gateRelay, LOW);
+}
+ 
+void turnOn(String deviceId) {
+  if (deviceId == "5eb808f763387c4ffbd38428" && digitalRead(ledClose)){ // If the device ID is compatible with that of the gate, and the gate is closed.
+    Serial.println("Opening Gate");
+    pulsegateRelay(); //Triggers the pulse
+  }
+}
+
+void turnOff(String deviceId) {
+   if (deviceId == "5eb808f763387c4ffbd38428" && digitalRead(ledOpen)) // If the device ID is compatible with that of the gate, and the gate is open.
+   {  
+     Serial.println("Closing gates");  
+     pulsegateRelay(); //Triggers the pulse
+   }
+}
+
+void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
+  switch(type) {
+    case WStype_DISCONNECTED:
+      isConnected = false;    
+      Serial.printf("[WSc] Webservice disconnected from sinric.com!\n");
+      break;
+    case WStype_CONNECTED: {
+      isConnected = true;
+      Serial.printf("[WSc] Service connected to sinric.com at url: %s\n", payload);
+      Serial.printf("Waiting for commands from sinric.com ...\n");        
+      }
+      break;
+    case WStype_TEXT: {
+        Serial.printf("[WSc] get text: %s\n", payload);
+        // Example payloads
+
+        // For Switch  types
+        // {"deviceId":"xxx","action":"action.devices.commands.OnOff","value":{"on":true}} // https://developers.google.com/actions/smarthome/traits/onoff
+        // {"deviceId":"xxx","action":"action.devices.commands.OnOff","value":{"on":false}}
+
+#if ARDUINOJSON_VERSION_MAJOR == 5
+        DynamicJsonBuffer jsonBuffer;
+        JsonObject& json = jsonBuffer.parseObject((char*)payload);
+#endif
+#if ARDUINOJSON_VERSION_MAJOR == 6        
+        DynamicJsonDocument json(1024);
+        deserializeJson(json, (char*) payload);      
+#endif        
+        String deviceId = json ["deviceId"];     
+        String action = json ["action"];
+        
+        if(action == "action.devices.commands.OnOff") { // Command received
+            String value = json ["value"]["on"];
+            Serial.println(value); 
+            
+            if(value == "true") {
+                turnOn(deviceId);
+            } else {
+                turnOff(deviceId);
+            }
+        }
+        else if (action == "test") {
+            Serial.println("[WSc] received test command from sinric.com");
+        }
+      }
+      break;
+    case WStype_BIN:
+      Serial.printf("[WSc] get binary length: %u\n", length);
+      break;
+    default: break;
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+  
+  WiFiMulti.addAP(MySSID, MyWifiPassword);
+  Serial.println();
+  Serial.print("Connecting to Wifi: ");
+  Serial.println(MySSID);  
+  pinMode(gateRelay, OUTPUT);
+  pinMode(ledClose, INPUT);
+  pinMode(ledOpen, INPUT);
+
+  // Waiting for Wifi connect
+  while(WiFiMulti.run() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  if(WiFiMulti.run() == WL_CONNECTED) {
+    Serial.println("");
+    Serial.print("WiFi connected. ");
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());
+  }
+
+  // server address, port and URL
+  webSocket.begin("iot.sinric.com", 80, "/"); //"iot.sinric.com", 80
+
+  // event handler
+  webSocket.onEvent(webSocketEvent);
+  webSocket.setAuthorization("apikey", MyApiKey);
+  
+  // try again every 5000ms if connection has failed
+  webSocket.setReconnectInterval(5000);   // If you see 'class WebSocketsClient' has no member named 'setReconnectInterval' error update arduinoWebSockets
+}
+
+void loop() {
+  webSocket.loop();
+  
+  if(isConnected) {
+      uint64_t now = millis();
+      
+      // Send heartbeat in order to avoid disconnections during ISP resetting IPs over night. Thanks @MacSass
+      if((now - heartbeatTimestamp) > HEARTBEAT_INTERVAL) {
+          heartbeatTimestamp = now;
+          webSocket.sendTXT("H");          
+      }
+  }   
+}
+ 
